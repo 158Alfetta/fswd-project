84148192e16a9852ac13a72e3817e603784f110c
satawatnack <s.nack1919@gmail.com>
2021-04-25 12:58:33 +0700

    add auth middleware

--- backend/src/graphql/queries/cart.js
+++ backend/src/graphql/queries/cart.js
@@ -1,4 +1,6 @@
 import { CartTC, productInCartTC } from '../../models'
+import { authQueryMiddleware } from './middleware'
 
 export const cart = CartTC.getResolver('findMany')
 export const productInCart = productInCartTC.getResolver('findMany')
+export const cartByUser = CartTC.getResolver('findMany', [authQueryMiddleware])
