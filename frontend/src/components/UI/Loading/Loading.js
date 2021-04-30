51f0be8b361ac7bff81f1d06ac1d09cb461c4869
satawatnack <s.nack1919@gmail.com>
2021-04-30 12:01:23 +0700

    .gitignore Fixed

--- frontend/src/components/UI/Loading/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import classes from './Loading.module.css'
-
-const Loading = (props) =>
-  props.showLoading ? <div className={classes.loader}>Loading...</div> : null
-
-export default Loading
