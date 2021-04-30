3c0015b7ea0761565f1b9ce8b6c412c64f802de9
TheNlesh <60070062@kmitl.ac.th>
2021-05-01 06:00:02 +0700

    Cart page decoration

--- frontend/src/components/Cart/getCart.js
+++ frontend/src/components/Cart/getCart.js
@@ -7,10 +7,10 @@ import { useSession } from '../../contexts/SessionContext'
 const GetCart = () => {
 
     const { user } = useSession()
-    const { data:CartData, refetch } = useQuery(QUERY_CART, { fetchPolicy: "no-cache" })
-
+    const { data: CartData, refetch } = useQuery(QUERY_CART, { fetchPolicy: "no-cache" })
+    // console.log(CartData)
     refetch()
-    
+
     const refetchQuery = {
         refetchQueries: () => [
             {
@@ -20,7 +20,7 @@ const GetCart = () => {
         ],
     }
 
-    const [updateCart] = useMutation(UPDATE_CART, refetchQuery)
+    const [updateCart] = useMutation(UPDATE_CART)
     var sum = 0
 
     function removeProduct(productId) {
@@ -70,71 +70,92 @@ const GetCart = () => {
         sum += a * b
         return a * b
     }
-    // return (
-    //     <div>{CartData.cart}</div>
-    // )
     return (
-        <div>
+        <>
+            <div className=" mx-8 my-5">
+                <div class="flex justify-between font-semibold text-2xl pt-10">
+                    <h1>{user?.username}'s Shopping Cart</h1>
+                    <h2>{CartData?.cart[0]?.product?.length} Item(s)</h2>
+                </div>
+
+                <div class="flex mt-10 mb-5">
+                    <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
+                    <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
+                    <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
+                    <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+                </div>
+            </div>
+
             {
                 CartData?.cart?.map((item) => {
                     return (
                         <div key={item}>
                             {item?.product?.map((product) => {
                                 return (
-                                    <div key={product?.productId}>
-                                        <span>
-                                            Product Name: {product?.productInfo?.name} (
-                      {product?.productInfo?.count} Remaining)
-                      <br />
-                                            <button
-                                                className="m-2 p-1 bg-gray-200 py-2 px-4 shadow-md hover:shadow-xl"
-                                                onClick={() => Decrease(product?.productId)}
-                                            >
-                                                {' '}
-                        -{' '}
-                                            </button>
-                      Quantity: {product?.quantity}
-                                            <button
-                                                className="m-2 p-1 bg-gray-200 py-2 px-4 shadow-md hover:shadow-xl"
-                                                onClick={() => Increase(product?.productId)}
-                                            >
-                                                {' '}
-                        +{' '}
-                                            </button>
-                                            <div>
-                                                <button
-                                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                                    onClick={() => removeProduct(product?.productId)}
-                                                >
-                                                    {' '}
-                          remove{' '}
-                                                </button>{' '}
+
+                                    <div class="flex items-center hover:bg-gray-100 mx-8 px-6 py-5 border-b">
+                                        <div class="flex w-2/5">
+                                            <div class="w-20">
+                                                <img style={{ width: '10vw' }} class="h-24" src={product?.productInfo?.image[0] || 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} />
                                             </div>
-                      Unitprice: {product?.productInfo?.price}
-                                            <br />
-                                            <b>
-                                                Total:{' '}
-                                                {summary(
-                                                    product?.productInfo?.price,
-                                                    product?.quantity
-                                                )}
-                                            </b>
-                                        </span>
+                                            <div class="flex flex-col justify-between ml-4 flex-grow">
+                                                <span class="font-bold text-sm">{product?.productInfo?.name}</span>
+                                                <span class="text-red-500 text-xs">{product?.productInfo?.count.toLocaleString()} Remaining</span>
+                                                <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs"> <button onClick={() => removeProduct(product?.productId)}> Remove </button></a>
+                                            </div>
+                                        </div>
+
+                                        <div class="flex justify-center w-1/5">
+                                            <span className="font-light"><button onClick={() => Decrease(product?.productId)}>-</button></span>
+
+                                            <input class="disabled:opacity-50 mx-2 border text-center w-8" type="text" value={product?.quantity.toLocaleString()} />
+
+                                            <span className="font-light"><button onClick={() => Increase(product?.productId)}>+</button></span>
+
+                                        </div>
+
+                                        <span class="text-center w-1/5 font-semibold text-sm">{parseFloat(product?.productInfo?.price).toLocaleString()}</span>
+                                        <span class="text-center w-1/5 font-semibold text-sm">{summary(product?.productInfo?.price, product?.quantity).toLocaleString()}</span>
+
+
+
+
                                     </div>
+
                                 )
                             })}
+                            <div id="summary" class="w-2/5 px-8 py-10" style={{float:"left"}}>
+                                <h1 class="font-semibold text-2xl pb-8">Order Summary</h1>
+                                <div class="flex justify-between mt-10 mb-5">
+                                    <span class="font-semibold text-sm uppercase">Item(s): {CartData?.cart[0]?.product?.length}</span>
+                                    <span class="font-semibold text-sm">{sum.toLocaleString()}</span>
+                                </div>
+                                <div>
+                                    <label class="font-medium inline-block mb-3 text-sm uppercase">Shipping</label>
+                                    <select class="block p-2 text-gray-600 w-full text-sm">
+                                        <option>Standard shipping - 50.00 THB </option>
+                                    </select>
+                                </div>
+
+                                <div class="border-t mt-8">
+                                    <div class="flex font-semibold justify-between py-6 text-sm uppercase">
+                                        <span>Total cost</span>
+                                        <span>{(sum + 50).toLocaleString()} Baht</span>
+                                    </div>
+                                    <a href="checkout"><button class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Checkout</button></a>
+                                </div>
+                                
+                                <a href="/products" class="flex font-semibold text-indigo-600 text-sm mt-10">
+                                    <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
+                                    Continue Shopping
+                                </a>
+                            </div>
+
                         </div>
                     )
                 })}
-            <b> Summary: {sum} </b> <br />
-            <a href="/checkout">
-                {' '}
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    {' '}
-          Proceed to checkout{' '}
-                </button>
-            </a>
-        </div>
+
+        </>
     )
 }
 
