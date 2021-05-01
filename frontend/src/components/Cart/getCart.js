a30dccf51ac8921fd24b6ce36e2b0a98d645e71d
Udomeak <60070120@it.kmitl.ac.th>
2021-05-01 11:35:54 +0700

    new checkout page

--- frontend/src/components/Cart/getCart.js
+++ frontend/src/components/Cart/getCart.js
@@ -80,9 +80,9 @@ const GetCart = () => {
 
                 <div class="flex mt-10 mb-5">
                     <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
-                    <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
-                    <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
-                    <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+                    <h3 class="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
+                    <h3 class="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
+                    <h3 class="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
                 </div>
             </div>
 
--- frontend/src/components/Checkout/Checkout.js
+++ frontend/src/components/Checkout/Checkout.js
@@ -5,7 +5,6 @@ import { QUERY_CART, QUERY_CART_ORDER} from "../../graphql/CartQuery";
 import { CLEAR_CART } from '../../graphql/CartMutation'
 import { useHistory } from 'react-router-dom'
 import { useSession } from '../../contexts/SessionContext'
-import CheckoutCard from './CheckoutCard'
 
 const Checkout = () => {
   const { user } = useSession();
@@ -45,18 +44,37 @@ const Checkout = () => {
   return (
     <>
       <div className="h-screen w-screen">
-        <div className="grid grid-cols-12 md:h-1/6 h-full pl-1 bg-red-200">
-          CheckoutPage
+        <div className="grid grid-cols-10 w-full h-20 mb-7">
+          <div className="col-span-3 border-b-2 border-yellow-800">
+            <h2 className="font-sans text-left py-6 px-10 font-semibold text-3xl">Checkout</h2>
+          </div>
         </div>
 
 
         
-        <div className="m-3 p-2 bg-red-200 ">
-          {data?.cart[0]?.product.map((product) => {
-            return (
-              <CheckoutCard product={product} />
-            )
-          })}
+        <div className="m-3 p-2 bg-green-200 bg-opacity-30 rounded-xl w-full md:w-8/12 mx-auto grid grid-cols-10">
+                  <h3 class="mt-2 p-1 pb-3 col-span-4  font-semibold text-gray-600 uppercase text-center border-gray-300 border-b-2">Product Details</h3>
+                  <h3 class="mt-2 p-1 pb-3 col-span-2 font-semibold text-gray-600 uppercase text-center border-gray-300 border-b-2 ">Quantity</h3>
+                  <h3 class="mt-2 p-1 pb-3 col-span-2 font-semibold text-gray-600 uppercase text-center border-gray-300 border-b-2 ">Price</h3>
+                  <h3 class="mt-2 p-1 pb-3 col-span-2 font-semibold text-gray-600 uppercase text-center border-gray-300 border-b-2 ">Total</h3>
+            {data?.cart[0]?.product.map((product) => {
+              return (
+                <>
+                <div className="col-span-4 h-36 border-r border-gray-300 border-b grid grid-cols-2">
+                    <div className="">
+                      <img className="p-2" src={product?.productInfo?.image[0] || 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} />
+                    </div>
+                  <p className="text-center pt-10">{product?.productInfo?.name}</p>
+                </div>
+                <div className="col-span-2 h-36 border-r border-gray-300 border-b">
+                    <p className="text-center pt-10">{product?.quantity}</p>
+                </div>
+                <div className="col-span-2 h-36 border-r border-gray-300 border-b">
+                    <p className="text-center pt-10" >{product?.productInfo?.price}</p>
+                </div>
+                </>
+              )
+            })}
         </div>
       </div>
 
