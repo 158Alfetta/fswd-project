d6359fd96ac0cd89f8cce70e5592f093bc81ce61
satawatnack <s.nack1919@gmail.com>
2021-05-03 00:15:21 +0700

    update index

--- frontend/src/components/Product/AddImage/AddImage.js
+++ frontend/src/components/Product/AddImage/AddImage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 import { v4 as uuidv4 } from 'uuid'
-import { storage } from '../../../firebase/firebase'
+import { storage } from '../../../firebase'
 import LoadingComponent from '../../UI/Loading/Loading'
 
 import addImg from '../../../img/addImg.png'
