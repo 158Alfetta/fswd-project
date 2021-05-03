4d81edd086a2004da63874b461087fa71fa87c76
TheNlesh <60070062@kmitl.ac.th>
2021-05-03 07:11:47 +0700

    Added promotion link from homepage

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -29,12 +29,13 @@ const Home = () => {
             </div>
           </div>
           <div className="col-span-12 md:col-span-7">
+            <a href="/promotions">
             <div
               className="h-full bg-cover bg-center"
               style={{
                 backgroundImage: `url('/img/homepage_promotion_1.jpg')`,
               }}
-            ></div>
+            ></div></a>
           </div>
         </div>
 
